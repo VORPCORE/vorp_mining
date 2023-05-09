7e7075692f41b921af3f66ce5f474f6c9bb31e42
cleisson <cleisson@outlook.com.br>
2023-05-09 11:38:40 -0300

    Improving identing code and translation

--- README.md
+++ README.md
@@ -3,11 +3,15 @@ Mining script
 ![Screenshot_10](https://user-images.githubusercontent.com/101003021/190602935-bec09f9a-4651-44b0-b2b9-3688f0a51df2.png)
 
 ## 1. Requirements
-- [VORP-Core](https://github.com/VORPCORE/vorp-core-lua)
-- syn_minigame
+
+- [VORP-Core GitHub](https://github.com/VORPCORE/vorp-core-lua)
+- [VORP-Core Discord](https://discord.com/invite/xhJRGhQFRr)
+- **NEED** [syn_minigame](https://cdn.discordapp.com/attachments/903875147050655744/906890251312721940/syn_minigame.rar)
 
 ## 2. insallation
-`ensure vorp_mining` in your resources.cfg
+
+- `ensure vorp_lumberjack` in your resources.cfg
+- `ensure syn_minigame` in your resources.cfg
 
 ## 3. features
 - Mine rocks and get items 
--- server/server.lua
+++ server/server.lua
@@ -2,84 +2,85 @@ VorpInv = exports.vorp_inventory:vorp_inventoryApi()
 
 local VorpCore = {}
 
-TriggerEvent("getCore",function(core)
-    VorpCore = core
+T = Translation.Langs[Lang]
+
+TriggerEvent("getCore", function(core)
+	VorpCore = core
 end)
 
 RegisterServerEvent("vorp_mining:pickaxecheck")
 AddEventHandler("vorp_mining:pickaxecheck", function(rock)
-	math.randomseed(os.time())
 	local _source = source
 	local miningrock = rock
 	local pickaxe = VorpInv.getItem(_source, Config.Pickaxe)
 	if pickaxe ~= nil then
-		local meta =  pickaxe["metadata"]
-		if next(meta) == nil then 
-			VorpInv.subItem(_source, Config.Pickaxe, 1,{})
-			VorpInv.addItem(_source, Config.Pickaxe, 1,{description = "Durability = 98",durability = 99})
+		local meta = pickaxe["metadata"]
+		if next(meta) == nil then
+			VorpInv.subItem(_source, Config.Pickaxe, 1, {})
+			VorpInv.addItem(_source, Config.Pickaxe, 1,
+				{ description = T.NotifyLabels.descDurabilityOne, durability = 99 })
 			TriggerClientEvent("vorp_mining:pickaxechecked", _source, miningrock)
 		else
 			local durability = meta.durability - 1
-			local description = "Durability = "
-			VorpInv.subItem(_source, Config.Pickaxe, 1,meta)
-			if 0 >= durability then 
-				local random = math.random(1,2)
-				if random == 1 then 
-					TriggerClientEvent("vorp:TipRight", _source, "Your pickaxe broke", 2000)
+			local description = T.NotifyLabels.descDurabilityTwo
+			VorpInv.subItem(_source, Config.Pickaxe, 1, meta)
+			if 0 >= durability then
+				local random = math.random(1, 2)
+				if random == 1 then
+					TriggerClientEvent("vorp:TipRight", _source, T.NotifyLabels.brokePickaxe, 2000)
 					TriggerClientEvent("vorp_mining:nopickaxe", _source)
 				else
-					VorpInv.addItem(_source, Config.Pickaxe, 1,{description = description.."1",durability = 1})
+					VorpInv.addItem(_source, Config.Pickaxe, 1, { description = description .. "1", durability = 1 })
 					TriggerClientEvent("vorp_mining:pickaxechecked", _source, miningrock)
 				end
 			else
-				VorpInv.addItem(_source, Config.Pickaxe, 1,{description = description..durability,durability = durability})
+				VorpInv.addItem(_source, Config.Pickaxe, 1,
+					{ description = description .. durability, durability = durability })
 				TriggerClientEvent("vorp_mining:pickaxechecked", _source, miningrock)
 			end
 		end
 	else
 		TriggerClientEvent("vorp_mining:nopickaxe", _source)
-		TriggerClientEvent("vorp:TipRight", _source, "You don't have a pickaxe", 2000)
+		TriggerClientEvent("vorp:TipRight", _source, T.NotifyLabels.notHavePickaxe, 2000)
 	end
 end)
 
 local keysx = function(table)
-    local keys = 0
-
-    for k,v in pairs(table) do
-       keys = keys + 1
-    end
-
-    return keys
+	local keys = 0
+	for k, v in pairs(table) do
+		keys = keys + 1
+	end
+	return keys
 end
 
 RegisterServerEvent('vorp_mining:addItem')
 AddEventHandler('vorp_mining:addItem', function()
-	--math.randomseed(os.time()) -- why s this even here? 
 	local _source = source
-	--local Character = VorpCore.getUser(_source).getUsedCharacter -- why is this even here ?
-	local chance =  math.random(1,20)
+	local chance = math.random(1, 20)
 	local reward = {}
-	for k,v in pairs(Config.Items) do 
+	
+	for k, v in pairs(Config.Items) do
 		if v.chance >= chance then
-			table.insert(reward,v)
+			table.insert(reward, v)
 		end
 	end
 
-        local randomtotal = keysx(reward) -- localize 
-	if randomtotal and randomtotal == 0  then -- if 0 add at least add 1 or maybe do a return
-		--randomtotal = 1 -- ensure its not 0 so it doesnt throw error, you can uncomment so players get at least one 
-           return -- dont run amount is 0 , comment if the top one is uncommented
+	local randomtotal = keysx(reward)
+	if randomtotal == 0 then
+		TriggerClientEvent("vorp:TipRight", _source, T.NotifyLabels.gotNothing, 3000)
+		return
 	end
-	local chance2 = math.random(1,randomtotal) -- if 0 the interval will be empty since minimum is 1
-	local count = math.random(1,reward[chance2].amount)
+
+	local chance2 = math.random(1, randomtotal) -- if 0 the interval will be empty since minimum is 1
+	local count = math.random(1, reward[chance2].amount)
 	TriggerEvent("vorpCore:canCarryItems", tonumber(_source), count, function(canCarry)
-		TriggerEvent("vorpCore:canCarryItem", tonumber(_source), reward[chance2].name,count, function(canCarry2)
+		TriggerEvent("vorpCore:canCarryItem", tonumber(_source), reward[chance2].name, count, function(canCarry2)
 			if canCarry and canCarry2 then
 				VorpInv.addItem(_source, reward[chance2].name, count)
-				TriggerClientEvent("vorp:TipRight", _source, "You found "..reward[chance2].label, 3000)
+				TriggerClientEvent("vorp:TipRight", _source, T.NotifyLabels.yourGot .. reward[chance2].label, 3000)
 			else
-				TriggerClientEvent("vorp:TipRight", _source, "You can't carry any more "..reward[chance2].label, 3000)
+				TriggerClientEvent("vorp:TipRight", _source, T.NotifyLabels.fullBag .. reward[chance2].label, 3000)
 			end
 		end)
-	end) 
+	end)
 end)
