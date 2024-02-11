43e04cdf11db2c73e15488ec2e34072cb850850b
Outsider <87246847+outsider31000@users.noreply.github.com>
2024-02-11 10:31:11 +0000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,21 +1,17 @@
-fx_version "adamant"
-
-games { "rdr3" }
-
+fx_version 'cerulean'
+game 'rdr3'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-client_scripts {
-   'client/client.lua'
-}
+author 'VORP @blue'
+description 'A mining script for vorp core framework'
+repository 'https://github.com/VORPCORE/vorp_mining'
 
 shared_scripts {
    'config.lua',
    'shared/*.lua'
 }
-
-server_scripts {
-   'server/server.lua'
-}
+client_scripts 'client/client.lua'
+server_script 'server/server.lua'
 
 dependencies {
    'syn_minigame'
