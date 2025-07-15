1e3ae47aeddcdc373da0e0977c40398cb03a267c
Outsider <87246847+outsider31000@users.noreply.github.com>
2025-07-15 13:12:51 +0100

    version 1.2

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -17,7 +17,7 @@ dependencies {
    'syn_minigame'
 }
 
-version '1.1'
+version '1.2'
 vorp_checker 'yes'
 vorp_name '^4Resource version Check^3'
 vorp_github 'https://github.com/VORPCORE/vorp_mining'
