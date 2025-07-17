4db1ab89cc4ebe7a54ffabe8f7e54b02cd29ab54
Outsider <87246847+outsider31000@users.noreply.github.com>
2025-07-17 20:33:59 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,7 @@
 fx_version 'cerulean'
 game 'rdr3'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
-
+lua54 'yes'
 author 'VORP @blue'
 description 'A mining script for vorp core framework'
 repository 'https://github.com/VORPCORE/vorp_mining'
